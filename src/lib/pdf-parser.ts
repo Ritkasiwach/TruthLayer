aeae82562c613e961df49ecc34b97e201ea5a6c1
Ritka_siwach0202 <rits.siwach2407@gmail.com>
2026-05-26 12:50:18 +0530

    fix: bypass pdf-parse buggy index file that tries to read test data

--- src/lib/pdf-parser.ts
+++ src/lib/pdf-parser.ts
@@ -6,7 +6,7 @@ if (typeof global.ImageData === 'undefined') global.ImageData = class ImageData 
 if (typeof global.Path2D === 'undefined') global.Path2D = class Path2D {} as any;
 
 // @ts-ignore
-const pdf = require("pdf-parse");
+const pdf = require("pdf-parse/lib/pdf-parse.js");
 
 export interface ParsedPDF {
   text: string;
